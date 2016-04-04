623284333281c9f103fa843180cb566181d3dad4
somkiat <somkiat.p@gmail.com>
2016-04-04 22:26:35 +0700

    Demo project

---
+++ app/src/main/kotlin/up1/hellokotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package up1.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        registerButton.setOnClickListener({
+            message.text = "Hello Kotlin on Android"
+        })
+    }
+}
